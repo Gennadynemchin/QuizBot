5b61833ee5ea1ca7db04ac92785a6224bb482bf9
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-11-29 12:13:57 +0300

    Added echo;

--- tg_bot.py
+++ tg_bot.py
@@ -1,69 +1,45 @@
 import logging
-import redis
-from enum import Enum, auto
-from get_question import get_random_question
-from telegram import ReplyKeyboardMarkup, Update, ForceReply
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, ConversationHandler, filters
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from credentials import telegram_token, redis_login, redis_password, redis_host
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
-)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-redis_db = redis.Redis(host=redis_host,
-                       port=14083,
-                       username=redis_login,
-                       password=redis_password,
-                       decode_responses=True)
+
+def start(bot, update):
+    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Показать результаты']]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text('Hi!', reply_markup=reply_markup)
 
 
-class State(Enum):
-    NEW_QUESTION = auto()
-    ANSWER = auto()
-    GIVE_UP = auto()
+def help(bot, update):
+    update.message.reply_text('Help!')
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    keyboard = [['New question', 'Give up'], ['Show result']]
-    markup = ReplyKeyboardMarkup(keyboard)
-    await update.message.reply_text('Hello and welcome!', reply_markup=markup)
-    return State.NEW_QUESTION
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
 
 
-async def new_question(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user.id
-    question, answer = get_random_question()
-    redis_db.set(str(user), answer)
-    await update.message.reply_text(question)
-    return State.ANSWER
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 
-async def get_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = str(update.effective_user.id)
-    user_text = update.message.text
-    answer = redis_db.get(user)
-    if user_text.lower() == answer.lower():
-        await update.message.reply_text("Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»")
-        return State.NEW_QUESTION
-    else:
-        await update.message.reply_text("Неправильно… Попробуешь ещё раз?")
-        return State.GIVE_UP
+def main():
+    updater = Updater(telegram_token)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
 
 
-def main() -> None:
-    application = Application.builder().token(telegram_token).build()
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            State.NEW_QUESTION: [MessageHandler(filters.Regex(r"New question"), new_question)],
-            State.ANSWER: [MessageHandler(filters.TEXT, get_answer)],
-            State.GIVE_UP: [MessageHandler(filters.TEXT, get_answer)]},
-        fallbacks=[])
-    application.add_handler(conv_handler)
-    application.run_polling()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
