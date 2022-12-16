6daccaf4aae27695ea9eb98ce8badf75d2743bfa
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-12-16 22:20:44 +0300

    Added vk_bot.py;

---
+++ vk_bot.py
@@ -0,0 +1,32 @@
+import random
+import vk_api as vk
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.utils import get_random_id
+from vk_api.longpoll import VkLongPoll, VkEventType
+from questions import vk_token
+
+
+def echo(event, vk_api, keyboard):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        keyboard=keyboard,
+        random_id=random.randint(1, 1000))
+
+
+if __name__ == "__main__":
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Белая кнопка', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Зелёная кнопка', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_line()
+    keyboard.add_button('Красная кнопка', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button('Синяя кнопка', color=VkKeyboardColor.PRIMARY)
+
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api, keyboard.get_keyboard())
