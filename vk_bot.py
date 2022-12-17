5dd58342e5f19601475377c6cbc4869dfb51e667
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-12-17 10:59:58 +0300

    Added keyboard vk_bot.py;

--- vk_bot.py
+++ vk_bot.py
@@ -1,31 +1,36 @@
-import random
 import vk_api as vk
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
-from questions import vk_token
+from questions import vk_token, \
+    get_random_question, \
+    save_user_question, \
+    create_new_user, \
+    check_user_answer, \
+    get_user_info, \
+    giveup_user, \
+    redis_login, \
+    redis_password, \
+    redis_host
 
 
-def echo(event, vk_api, keyboard):
+def quiz_bot(event, vk_api):
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button('Показать результаты', color=VkKeyboardColor.SECONDARY)
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
-        keyboard=keyboard,
-        random_id=random.randint(1, 1000))
+        keyboard=keyboard)
+
 
 
 if __name__ == "__main__":
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
-
-    keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button('Белая кнопка', color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button('Зелёная кнопка', color=VkKeyboardColor.POSITIVE)
-    keyboard.add_line()
-    keyboard.add_button('Красная кнопка', color=VkKeyboardColor.NEGATIVE)
-    keyboard.add_line()
-    keyboard.add_button('Синяя кнопка', color=VkKeyboardColor.PRIMARY)
-
     longpoll = VkLongPoll(vk_session)
+
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api, keyboard.get_keyboard())
+            quiz_bot(event, vk_api)
