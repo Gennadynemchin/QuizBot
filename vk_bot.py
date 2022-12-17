f3d60d180b0ee06062a112a12fbd395a74e67a57
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-12-17 20:02:27 +0300

    Added conversation handlers for vk_bot.py;

--- vk_bot.py
+++ vk_bot.py
@@ -13,23 +13,34 @@ from questions import vk_token, \
     redis_host
 
 
-def quiz_bot(event, vk_api):
-    keyboard = VkKeyboard(one_time=True)
+def quiz_bot(vk_longpoll, vk_api):
+    keyboard = VkKeyboard(one_time=False)
     keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
     keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
     keyboard.add_line()
     keyboard.add_button('Показать результаты', color=VkKeyboardColor.SECONDARY)
-    vk_api.messages.send(user_id=event.user_id,
-                         message=event.text,
-                         random_id=0,
-                         keyboard=keyboard.get_keyboard())
+
+    for event in vk_longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            if event.text == 'Новый вопрос':
+                vk_api.messages.send(user_id=event.user_id,
+                                     message='NEW QUESTION',
+                                     random_id=0,
+                                     keyboard=keyboard.get_keyboard())
+            elif event.text == 'Сдаться':
+                vk_api.messages.send(user_id=event.user_id,
+                                     message='GIVE UP',
+                                     random_id=0,
+                                     keyboard=keyboard.get_keyboard())
+            elif event.text == 'Показать результаты':
+                vk_api.messages.send(user_id=event.user_id,
+                                     message='SHOW RESULTS',
+                                     random_id=0,
+                                     keyboard=keyboard.get_keyboard())
 
 
 if __name__ == "__main__":
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
-
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            quiz_bot(event, vk_api)
+    vk_longpoll = VkLongPoll(vk_session)
+    quiz_bot(vk_longpoll, vk_api)
