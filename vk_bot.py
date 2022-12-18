1ce86b7fe488a84a082565b83501ac00be647910
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2022-12-18 16:00:13 +0300

    Added conversation handlers for vk_bot.py;

--- vk_bot.py
+++ vk_bot.py
@@ -8,13 +8,11 @@ from questions import vk_token, \
     check_user_answer, \
     get_user_info, \
     giveup_user, \
-    redis_login, \
-    redis_password, \
-    redis_host, \
     redis_db
 
 
 messenger = 'vk'
+
 
 def quiz_bot(vk_longpoll, vk_api):
     keyboard = VkKeyboard(one_time=False)
@@ -22,11 +20,10 @@ def quiz_bot(vk_longpoll, vk_api):
     keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
     keyboard.add_line()
     keyboard.add_button('Показать результаты', color=VkKeyboardColor.SECONDARY)
-
+# create_new_user(redis_db, 'vk', user)
     for event in vk_longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             user = event.user_id
-            create_new_user(redis_db, 'vk', user)
             if event.text == 'Новый вопрос':
                 question, answer = get_random_question()
                 save_user_question(redis_db, messenger, user, question, answer)
@@ -34,13 +31,6 @@ def quiz_bot(vk_longpoll, vk_api):
                                      message=question,
                                      random_id=0,
                                      keyboard=keyboard.get_keyboard())
-                else:
-                    user_answer = event.text.lower()
-                    result = check_user_answer(redis_db, messenger, user, user_answer)
-                    if result:
-                        print(f"{user_answer} 'Абсолютно верно!'")
-                    else:
-                        print(f"{user_answer} 'Это неправильный ответ. Попробуй еще раз'")
             elif event.text == 'Сдаться':
                 message = giveup_user(redis_db, messenger, user)
                 print(message)
@@ -54,8 +44,19 @@ def quiz_bot(vk_longpoll, vk_api):
                                      message=f'Верных ответов: {correct_answers}\nВсего ответов: {total_answers}',
                                      random_id=0,
                                      keyboard=keyboard.get_keyboard())
-
-
+            else:
+                user_answer = event.text.lower()
+                result = check_user_answer(redis_db, messenger, user, user_answer)
+                if result:
+                    vk_api.messages.send(user_id=user,
+                                         message=f"{user_answer} 'Абсолютно верно!'",
+                                         random_id=0,
+                                         keyboard=keyboard.get_keyboard())
+                else:
+                    vk_api.messages.send(user_id=user,
+                                         message=f"{user_answer} 'Это неправильный ответ. Попробуй еще раз'",
+                                         random_id=0,
+                                         keyboard=keyboard.get_keyboard())
 
 
 if __name__ == "__main__":
